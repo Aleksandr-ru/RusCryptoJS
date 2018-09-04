04e997a2b138868c3c6c597795931018d9abe002
Aleksandr <aleksandr@aleksandr.ru>
2018-09-04 13:54:36 +0300

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,55 @@
 'use strict';
 
 var webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var WebpackCopyAfterBuildPlugin = require('webpack-copy-after-build-plugin');
 var path = require('path');
 var libraryName = 'RusCryptoJS';
+var filename = 'ruscrypto.min.js';
 
-module.exports = {
-    entry: {
-        vendor: [
-            'babel-polyfill'
-        ], 
-        main: './src'
-    },
-    output: {
-        path: __dirname,
-        filename: './dist/ruscrypto.min.js',
-        library: libraryName,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    module: {
-        rules: [{
-            test: /\.js$/, 
-            loaders: ['babel-loader'], 
-            exclude: /node_modules/
-        }],
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new CopyWebpackPlugin([{
-            'from': './dist/ruscrypto.min.js',
-            'to': path.resolve('./docs/js')
-        }])
-    ],
-    devtool: "source-map",
-    devServer: {
-        hot: true
+module.exports = (env, argv) => {
+    if (argv.mode === 'production') {
+        var plugins = [
+            new CleanWebpackPlugin([
+                './dist'
+            ]),
+            new WebpackCopyAfterBuildPlugin({
+                main: '../docs/js/' + filename
+            })
+        ];
+    }
+    else {
+        var plugins = [
+            new webpack.HotModuleReplacementPlugin()
+        ];
+    }
+    return {
+        entry: {
+            main: [ 'babel-polyfill', './src' ]
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: filename,
+            library: libraryName,
+            libraryTarget: 'umd',
+            umdNamedDefine: true,
+            // https://github.com/webpack/webpack/issues/1625#issuecomment-407553490
+            globalObject: "typeof self !== 'undefined' ? self : this"
+        },
+        resolve: {
+            extensions: ['.js'],
+        },
+        module: {
+            rules: [{
+                test: /\.js$/, 
+                loaders: ['babel-loader'], 
+                exclude: /node_modules/
+            }],
+        },
+        plugins: plugins,
+        devtool: "source-map",
+        devServer: {
+            hot: true
+        }
     }
 };
